c6c211951f9a4ddaee5357efaeafad832f730460
lol <user02@work>
2015-01-22 16:52:58 +0100

    Nic ciekawego

--- bjbs.py
+++ bjbs.py
@@ -2,8 +2,10 @@
 
 import random
 import numpy as np
+import matr
+import os
 
-DL1=0
+DL1=0 #do not change values to numerical fot these variables which do not have _v in their name
 DL1_v=0
 DL2=0
 DL2_v=0
@@ -27,27 +29,42 @@ def numerical_value(card):
 
 #print values
 def print_val():
+    print os.system("clear")
     print "DEALER:"
     print "%s" % DL1
     print "\nPLAYER:"
     print "%s %s" % (PL1, PL2)
 
-#check which matrice is valid, precheck has to be for bj
-def check_matrice(PL1, DL1, DL2):
+#check which matrice is valid, precheck has to done to determine if there is bj
+def check_matrice(pl1, pl2):
     if PL1 == PL2:
-        return 3
+        return "matrice_c.dat"
     elif (PL1 == "A") or (PL2 == "A"):
-        return 2
+        return "matrice_b.dat"
     else:
-        return 1
+        return "matrice_a.dat" 
 
+#check for proper play
+def prop_play(pl1, pl2, dl1):
+    check_matrice(pl1, pl2)
+    if check_matrice(pl1, pl2) == "matrice_a.dat":
+        return matr.ch_matrice_a(dl1, pl1, pl2)
+    elif check_matrice(pl1, pl2) == "matrice_b.dat":
+        return matr.ch_matrice_b(dl1, pl1, pl2)
+    elif check_matrice(pl1, pl2) == "matrice_c.dat":
+        return matr.ch_matrice_c(dl1, pl1, pl2) 
+    else: 
+        print "Error with determination of proper play"
+
+#toss for random cards
 DL1 = random.choice(deck)
+DL2 = random.choice(deck)
 PL1 = random.choice(deck)
 PL2 = random.choice(deck)
 
+#get numerical values of cards
 DL1_v = numerical_value(DL1)
 DL2_v = numerical_value(DL2)
-
 PL1_v = numerical_value(PL1)
 PL2_v = numerical_value(PL2)
 
@@ -56,4 +73,15 @@ print_val()
 PL_V = PL1_v + PL2_v #calculates overall numerical value for player
 DL_V = DL1_v + DL2_v #calculates overall numerical value for dealer
 
-print check_matrice(PL1, DL1, DL2)
+#check which matrice to use
+#print "Proper matrice: "
+#print check_matrice(PL1, PL2)
+
+print "\nProper play: "
+#print str.center(12, 'Proper play:')
+#str = "this is string example....wow!!!";
+#print str.center(100)
+
+
+print prop_play(PL1_v, PL2_v, DL1_v)
+print prop_play(10, 10, 10)
---
+++ matr.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+#assume that all figure (except A) are transfered to 10 value
+#precheck of matrice is required
+
+import math
+import numpy as np
+
+DL1 = "A"
+PL1 = "A"
+PL2 = "A"
+
+def ch_matrice_a(pl1, pl2, dl1):
+    data = np.genfromtxt("matrice_a.dat",delimiter="|",dtype=str) #get matrice
+    pl_v = pl1 + pl2 #take sum, assume that there are no A or other fig.
+
+    if dl1 == "A": #check if dealer card is A, then assign value
+        dl1 = 11
+
+    if pl1 == "A":
+        pl1 = 11
+    elif pl2 == "A":
+        pl2 = 11
+    elif pl_v > 17:
+        pl_v = 17
+    else: 
+        True
+    
+    if dl1 == "A":
+        dl1 = 11
+
+#    print data[pl_v - 4][dl1 - 1]
+    return data[pl_v - 4][dl1 - 1]
+
+def ch_matrice_b(pl1, pl2, dl1):
+    data = np.genfromtxt("matrice_b.dat",delimiter="|",dtype=str) #get matrice
+    
+    if dl1 == "A": #check if dealer card is A
+        dl1 = 11
+    
+    #check which card is A
+    if pl1 == "A":
+#        print data[pl2 - 1][dl1 - 1]
+        return data[pl2 - 1][dl1 - 1]
+    elif pl2 == "A":
+#        print data[pl1 - 1][dl1 - 1]
+        return data[pl1 - 1][dl1 - 1]
+    else: 
+        True
+
+
+def ch_matrice_c(pl1, pl2, dl1):
+    data = np.genfromtxt("matrice_c.dat",delimiter="|",dtype=str) #get matrice
+
+    if dl1 == "A":
+        dl1 = 11
+
+    if pl1 == "A": #check if cards are As
+        pl1 = 1
+    else: 
+        True
+
+#    print data[pl1][dl1 - 1]
+    return data[pl1][dl1 - 1]
+
+#ch_matrice_b(DL1, PL1, PL2)
+print ch_matrice_a(10, 2, 3)
